02f3aebb49dd991bfc22a0efcd31e9f106306c03
BlackWal3a <mednejib12@gmail.com>
2023-12-16 00:38:35 +0100

    Deployment

---
+++ ecommerce-app/config/site.ts
@@ -0,0 +1,14 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "Commercy",
+  description: "Site web développé par Nejib & Khaled",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ],
+}
